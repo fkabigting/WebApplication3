272316b6ffb712067259073b3802b70d27c070b5
fkabigting <leonardfrancis.kabigting1@my-aolcc.com>
2024-01-25 23:35:28 -0700

    Add project files.

---
+++ Controllers/ServerTimeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class ServerTimeController : Controller
+    {
+        public IActionResult Time()
+        {
+            return View();
+        }
+    }
+
+
+
+}
